1f42a3159619b2c5f80a33dbb74c32c51be7daa0
vadim l <vadimlozinschii@gmail.com>
2023-03-21 07:08:07 -0700

    adding multiplication, random num greater than 1, division, max num

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = (num1 * num2);
+multiply;
+
+let random = Math.floor(Math.random() ) + 1;
+random;
+
+
+const num3 = 16;
+const num4 = 4;
+let mod = (num3 / num4);
+mod;
+
+let max = Math.max(20);
+max;
